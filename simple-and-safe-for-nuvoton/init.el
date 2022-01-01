050a0e9ef8254efb8cd13e0d24de18cf0fb1cc5f
Ivanir Efi <efi.ivanir@gmail.com>
2022-01-01 15:27:20 +0200

    Add simple-and-safe-for-nuvoton

---
+++ simple-and-safe-for-nuvoton/init.el
@@ -0,0 +1,162 @@
+;; (add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; (require 'xah-fly-keys)
+
+;; ;; specify a layout
+;; (xah-fly-keys-set-layout "qwerty")
+
+;; (xah-fly-keys 1)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'paren)
+
+(setq show-paren-delay 0)           ; how long to wait?
+(show-paren-mode t)                 ; turn paren-mode on
+(setq show-paren-style 'expression) ; alternatives are 'parenthesis' and 'mixed'
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq dired-listing-switches "-lt")
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+					; Perl
+ (mapc
+     (lambda (pair)
+       (if (eq (cdr pair) 'perl-mode)
+           (setcdr pair 'cperl-mode)))
+     (append auto-mode-alist interpreter-mode-alist))
+
+(setq auto-mode-alist (cons '("\\.gsr$" . cperl-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.t$" . cperl-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.pl$" . cperl-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.t$" . cperl-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.cgi$" . cperl-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.psgi$" . cperl-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("cpanfile$" . cperl-mode) auto-mode-alist))
+
+(setq cperl-font-lock t)
+
+(add-hook 'cperl-mode-hook
+  (lambda ()
+    (setq cperl-hairy nil
+      abbrev-mode t     ;; automatic keyword expansion
+      cperl-highlight-variables-indiscriminately t
+      cperl-auto-newline t
+      cperl-auto-newline-after-colon t
+      cperl-regexp-scan nil
+
+      indent-tabs-mode nil
+      cperl-close-paren-offset -4
+      cperl-continued-statement-offset 4
+      cperl-indent-level 4
+      cperl-indent-parens-as-block t
+      cperl-tab-always-indent t
+      cperl-indent-parens-as-block t
+      
+      cperl-electric-keywords t 
+      cperl-electric-linefeed t  
+      cperl-electric-parens nil)))
+ 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; By default Emacs triggers garbage collection at ~0.8MB which makes
+;; startup really slow. Since most systems have at least 64MB of memory,
+;; we increase it during initialization.
+(setq gc-cons-threshold 64000000)
+(add-hook 'after-init-hook #'(lambda ()
+                               ;; restore after startup
+                               (setq gc-cons-threshold 800000)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General Tweaks
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; turn on highlight matching brackets when cursor is on one
+(show-paren-mode t)
+;; Overwrite region selected
+(delete-selection-mode t)
+;; Show column numbers by default
+(setq column-number-mode t)
+;; Use CUA to delete selections
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+(transient-mark-mode 1) ;; No region when it is not highlighted
+(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
+
+;; Prevent emacs from creating a bckup file filename~
+(setq make-backup-files nil)
+;; Settings for searching
+(setq-default case-fold-search t ;case insensitive searches by default
+              search-highlight t) ;hilit matches when searching
+;; Highlight the line we are currently on
+(global-hl-line-mode t)
+
+
+;; Auto-wrap at 80 characters
+(setq-default auto-fill-function 'do-auto-fill)
+(setq-default fill-column 80)
+(turn-on-auto-fill)
+;; Disable auto-fill-mode in programming mode
+(add-hook 'prog-mode-hook (lambda () (auto-fill-mode -1)))
+
+;; Global Keyboard Shortcuts
+;; Set help to C-?
+(global-set-key (kbd "C-?") 'help-command)
+;; Set mark paragraph to M-?
+(global-set-key (kbd "M-?") 'mark-paragraph)
+;; Set backspace to C-h
+(global-set-key (kbd "C-h") 'delete-backward-char)
+;; Set backspace word to M-h
+(global-set-key (kbd "M-h") 'backward-kill-word)
+;; Use meta+tab word completion
+(global-set-key (kbd "M-TAB") 'dabbrev-expand)
+;; Easy undo key
+(global-set-key (kbd "C-/") 'undo)
+;; Comment or uncomment the region
+(global-set-key (kbd "C-c ;") 'comment-or-uncomment-region)
+;; Indent after a newline, if required by syntax of language
+(global-set-key (kbd "C-m") 'newline-and-indent)
+;; Undo, basically C-x u
+(global-set-key (kbd "C-/") 'undo)
+;; Find file in project
+(global-set-key (kbd "C-x M-f") 'project-find-file)
+
+;; We don't want to type yes and no all the time so, do y and n
+(defalias 'yes-or-no-p 'y-or-n-p)
+;; Disable the horrid auto-save
+(setq auto-save-default nil)
+(setq auto-save-list-file-prefix nil)
+
+;; Don't ring the bell
+(setq ring-bell-function 'ignore)
+
+;; Non-nil means draw block cursor as wide as the glyph under it.
+;; For example, if a block cursor is over a tab, it will be drawn as
+;; wide as that tab on the display.
+(setq x-stretch-cursor t)
+
+;; Dont ask to follow symlink in git
+(setq vc-follow-symlinks t)
+
+
+;; Set default window size and position
+(setq default-frame-alist
+      '((top . 0) (left . 0) ;; position
+        (width . 110) (height . 90) ;; size
+        ))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(show-paren-mode t))
+
+(set-face-attribute 'region nil :background "yellow")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 162 :width normal)))))
